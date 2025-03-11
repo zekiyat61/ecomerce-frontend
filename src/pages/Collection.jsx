b918b554374585a507d7bdbbfdd5b0d32219df73
zekiyat <zekiyatatsh2009@gmail.com>
2025-03-11 09:24:24 +0300

    second commit

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState, useCallback } from "react";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets";
 import Title from "../components/Title";
-import ProductItem from "../components/ProductItem";
+import ProductItem from "../components/productItem";
 
 const Collection = () => {
   const { products, search, showSearch } = useContext(ShopContext);
